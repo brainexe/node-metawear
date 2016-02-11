80e787aac10b2546c14537a43d7a99f9e4ba2579
Alan Hortz <alan.hortz@gmail.com>
2016-02-12 00:08:08 +0100

    Add the correct test implementation for node 0.10 to be able to execute the tests

--- tests/spec/registers/accelerometerSpec.js
+++ tests/spec/registers/accelerometerSpec.js
@@ -1,9 +1,16 @@
 var Accelerometer = require('../../../src/registers/accelerometer'),
-    Device   = require('../helpers/device');
+    Device   = require('../helpers/device'),
+    bufferEqual = require('buffer-equal');
+
+
+
+
 
 describe("Accelerometer", function() {
 		var device = new Device(),
 				accelerometer = new Accelerometer(device);
+
+
 
 		it("should have 50hz as default value for output data rate", function() {
 			expect(accelerometer.dataRate).toEqual(0x7);
@@ -18,15 +25,14 @@ describe("Accelerometer", function() {
 			beforeEach(function() {
 				//device = new Device();
 				spyOn(device, 'send').and.callThrough();
+				jasmine.addCustomEqualityTester(bufferEqual);
 			});
 
 			it("should send the configured output data rate and axis sampling range to the MetaWear device", function() {
 				accelerometer.setConfig();
 				expect(device.send).toHaveBeenCalled();
-				expect(Buffer.compare(
-						device.buffers.pop(),
-						new Buffer([0x3,0x3,0x27,0x3]))
-				).toEqual(0); // Buffer.compare returns 0 when the two buffers are equal !
+
+				expect(device.buffers.pop()).toEqual(new Buffer([0x3,0x3,0x27,0x3]));
 			});
 		});
 				
