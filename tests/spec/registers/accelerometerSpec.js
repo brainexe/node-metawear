e55c590ab23d3b1eef1e1f00273d7866935a5b3b
Alan Hortz <alan.hortz@gmail.com>
2016-02-11 23:13:43 +0100

    Adding the spec for the method setConfig()

--- tests/spec/registers/accelerometerSpec.js
+++ tests/spec/registers/accelerometerSpec.js
@@ -12,6 +12,23 @@ describe("Accelerometer", function() {
 		it("should have +-2g as default axis sampling range", function() {
 			expect(accelerometer.accRange).toEqual([0x3,    16384]);
 		});
+
+		describe("setConfig()", function() {
+			
+			beforeEach(function() {
+				//device = new Device();
+				spyOn(device, 'send').and.callThrough();
+			});
+
+			it("should send the configured output data rate and axis sampling range to the MetaWear device", function() {
+				accelerometer.setConfig();
+				expect(device.send).toHaveBeenCalled();
+				expect(Buffer.compare(
+						device.buffers.pop(),
+						new Buffer([0x3,0x3,0x27,0x3]))
+				).toEqual(0); // Buffer.compare returns 0 when the two buffers are equal !
+			});
+		});
 				
 });
 
