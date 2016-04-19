a25916f803429003328beb80c75eb214e03d93d9
matze <matze@mdoetsch.de>
2016-04-19 21:03:45 +0200

    further implementation of GPIO register

---
+++ examples/gpio.js
@@ -0,0 +1,18 @@
+
+var devices  = require('./../src/device');
+
+devices.discover(function(device) {
+    console.log('discovered device ', device.address);
+
+    device.on('disconnect', function() {
+        console.log('we got disconnected! :( ');
+    });
+
+    device.connectAndSetup(function(error) {
+        console.log('were connected!');
+
+        var gpio = new device.Gpio(device);
+
+        // todo
+    });
+});
--- src/registers/gpio.js
+++ src/registers/gpio.js
@@ -4,13 +4,11 @@ const MODULE_OPCODE = 0x05;
 /**
  * Useful android references:
  *
- * GPI interface: https://github.com/mbientlab/Metawear-AndroidAPI/blob/04a18a8b3eb79669a3b3f96951953ff1991c26d6/library/src/main/java/com/mbientlab/metawear/module/Gpio.java
+ * GPIO interface: https://github.com/mbientlab/Metawear-AndroidAPI/blob/04a18a8b3eb79669a3b3f96951953ff1991c26d6/library/src/main/java/com/mbientlab/metawear/module/Gpio.java
  * GPIO implementation: https://github.com/mbientlab/Metawear-AndroidAPI/blob/04a18a8b3eb79669a3b3f96951953ff1991c26d6/library/src/main/java/com/mbientlab/metawear/impl/DefaultMetaWearBoard.java#L3986
  * Register definition: https://github.com/mbientlab/Metawear-AndroidAPI/blob/26e2eee76c0d15ee68af01b8c5c7ddab96a532c6/library/src/main/java/com/mbientlab/metawear/impl/characteristic/GpioRegister.java
  *
  * For reading data the file "switch.js" is an easy example...just use "this.device.emitter"
- * 
- * 
  */
 
 const
@@ -23,7 +21,7 @@ const
     READ_AI_ADC     = 0x7,
     READ_DI         = 0x8,
     PIN_CHANGE      = 0x9,
-    PIN_CHANGE_NOTIFY = 0xa,
+    PIN_CHANGE_NOTIFY = 0xa, // ResponseHeader header= new ResponseHeader(response[0], response[1], response[2]);
     PIN_CHANGE_NOTIFY_ENABLE = 0xb;
 
 var Gpio = function(device) {
@@ -40,45 +38,177 @@ var Gpio = function(device) {
         NO_PULL   : 2
     };
     this.PIN_CHANGE_TYPE = {
-        RISING   : 0,
-        FALLING  : 1,
-        ANY      : 2
+        RISING   : 2,
+        FALLING  : 2,
+        ANY      : 3
     };
 };
 
-Gpio.prototype.readAnalogIn = function(pin, readMode, silent) {
-    // todo
-};
-Gpio.prototype.setPinPullMode = function(pin, pullMode) {
-    // todo
-};
-Gpio.prototype.readDigitalIn = function(pin, silent) {
-    // todo
-};
-Gpio.prototype.setDigitalOut = function(pin) {
-    // todo
-};
-Gpio.prototype.clearDigitalOut = function(pin) {
-    // todo
-};
-Gpio.prototype.setPinChangeType = function(pin, pinChangeType) {
-    // todo
+/**
+ * @param {number} pin
+ * @param {number} readMode
+ * @param {function} callback
+ * @param {boolean} silent
+ */
+Gpio.prototype.readAnalogIn = function(pin, readMode, callback, silent) {
+    var method;
+    if (readMode == this.ANALOG_READ_MODE.ADC) {
+        method = READ_AI_ABS_REF;
+    } else {
+        method = READ_AI_ADC;
+    }
+
+    var buffer = new Buffer(4);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = method;
+    buffer[2] = silent ? 0 : 1;
+    buffer[3] = pin;
+
+    this.device.emitter.once([MODULE_OPCODE, method], function(buffer) {
+        // todo https://github.com/mbientlab/Metawear-AndroidAPI/blob/04a18a8b3eb79669a3b3f96951953ff1991c26d6/library/src/main/java/com/mbientlab/metawear/impl/DefaultMetaWearBoard.java#L3060
+        callback(buffer);
+    });
+
+    this.device.sendRead(buffer);
 };
 
-Gpio.prototype.startPinChangeDetection = function(pin) {
+/**
+ * @param {number} pin
+ * @param {number} pullMode
+ */
+Gpio.prototype.setPinPullMode = function(pin, pullMode) {
+    var method;
+    switch (pullMode) {
+        case this.PULL_MODE.PULL_UP:
+            method = PULL_UP_DI;
+            break;
+        case this.PULL_MODE.PULL_DOWN:
+            method = PULL_DOWN_DI;
+            break;
+        case this.PULL_MODE.NO_PULL:
+            method = NO_PULL_DI;
+            break;
+    }
+
     var buffer = new Buffer(3);
     buffer[0] = MODULE_OPCODE;
-    buffer[1] = pin;
-    buffer[2] = 1;
+    buffer[1] = method;
+    buffer[2] = pin;
 
     this.device.send(buffer);
 };
 
+/**
+ * @param {number} pin
+ * @param {function} callback
+ * @param {boolean} silent
+ */
+Gpio.prototype.readDigitalIn = function(pin, callback, silent) {
+    var buffer = new Buffer(4);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = READ_DI;
+    buffer[2] = silent ? 0 : 1;
+    buffer[3] = pin;
+
+    this.device.sendRead(buffer);
+
+    this.device.emitter.once([MODULE_OPCODE, READ_DI], function(buffer) {
+        // TODO
+        callback(buffer);
+    });
+};
+
+/**
+ * @param {number} pin
+ */
+Gpio.prototype.setDigitalOut = function(pin) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = SET_DO;
+    buffer[2] = pin;
+
+    this.device.send(buffer);
+};
+
+/**
+ * @param {number} pin
+ */
+Gpio.prototype.clearDigitalOut = function(pin) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = CLEAR_DO;
+    buffer[2] = pin;
+
+    this.device.send(buffer);
+};
+
+/**
+ * @param {number} pin
+ * @param {number} pinChangeType
+ */
+Gpio.prototype.setPinChangeType = function(pin, pinChangeType) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = PIN_CHANGE;
+    buffer[2] = pin;
+    buffer[3] = pinChangeType;
+
+    this.device.send(buffer);
+};
+
+/**
+ * @param {number} pin
+ */
+Gpio.prototype.startPinChangeDetection = function(pin) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = PIN_CHANGE_NOTIFY_ENABLE;
+    buffer[2] = pin;
+    buffer[3] = 1;
+
+    this.device.send(buffer);
+};
+
+/**
+ * @param {number} pin
+ */
 Gpio.prototype.stopPinChangeDetection = function(pin) {
     var buffer = new Buffer(3);
     buffer[0] = MODULE_OPCODE;
-    buffer[1] = pin;
-    buffer[2] = 0;
+    buffer[1] = PIN_CHANGE_NOTIFY_ENABLE;
+    buffer[2] = pin;
+    buffer[3] = 0;
+
+    this.device.send(buffer);
+};
+
+/**
+ * @param {number} pin
+ */
+Gpio.prototype.enableNotifications = function(pin, callback) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = PIN_CHANGE_NOTIFY;
+    buffer[2] = pin;
+    buffer[3] = 1;
+
+    this.device.send(buffer);
+
+    this.device.emitter.on([MODULE_OPCODE, PIN_CHANGE_NOTIFY], function(buffer) {
+        // TODO
+        callback(buffer);
+    });
+};
+
+/**
+ * @param {number} pin
+ */
+Gpio.prototype.unsubscribe = function(pin) {
+    var buffer = new Buffer(3);
+    buffer[0] = MODULE_OPCODE;
+    buffer[1] = PIN_CHANGE_NOTIFY;
+    buffer[2] = pin;
+    buffer[3] = 0;
 
     this.device.send(buffer);
 };
