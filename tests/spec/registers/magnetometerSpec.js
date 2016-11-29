e0c62678f587360cc60ae7157347260c4100058b
Alan Hortz <alan.hortz@gmail.com>
2016-11-29 14:34:19 +0100

    Update magnetometerSpec.js

--- tests/spec/registers/magnetometerSpec.js
+++ tests/spec/registers/magnetometerSpec.js
@@ -1,1 +1,8 @@
 // Magnetometer Specifications
+var Magnetometer = require('../../../src/registers/magnetometer'),
+    Device   = require('../helpers/device'),
+    bufferEqual = require('buffer-equal');
+
+describe("Magnetometer", function() {
+
+});
