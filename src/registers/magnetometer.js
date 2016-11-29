3fb1399b00b1fc172a64ad7a7cdb0d65f1149f1b
Alan Hortz <alan.hortz@gmail.com>
2016-11-29 14:52:51 +0100

    Update magnetometer.js

--- src/registers/magnetometer.js
+++ src/registers/magnetometer.js
@@ -1,1 +1,21 @@
+/*jslint es6 node*/
+
+const MODULE_OPCODE = 0x15;
+
+const POWER_MODE = 0x1,
+      DATA_INTERRUPT_ENABLE = 0x2,
+      DATA_RATE = 0x3,
+      DATA_REPETITIONS = 0x4,
+      MAG_DATA = 0x5,
+      THRESHOLD_INTERRUPT_ENABLE = 0x6,
+      THRESHOLD_CONFIG = 0x7,
+      THRESHOLD_INTERRUPT = 0x8,
+      PACKED_MAG_DATA = 0x9;
+
+
+var Magnetometer = function (device) {
+    this.device = device;
+};
+
+module.exports = Magnetometer;
 
